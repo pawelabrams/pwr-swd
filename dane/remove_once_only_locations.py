8509d1e514d542e8c9a72c9d86f97b4343d0ac0c
Paweł Abramowicz <pawelabrams@gmail.com>
2016-06-06 23:04:35 +0200

    Usunięte dodatkowe duplikaty

--- dane/remove_once_only_locations.py
+++ dane/remove_once_only_locations.py
@@ -6,13 +6,15 @@ points_twice = set()
 with open('locationsNested.json') as fp:
     teams = json.load(fp)
     for t in teams:
+        last = None
         for l in t:
             p = (float(l['latitude'][0:5]), float(l['longitude'][0:5]))
-            if p in points:
+            if p in points and p != last:
                 if not p in points_twice:
                     points_twice.add(p)
                     #print (p)
             else:
                 points.add(p)
+            last = p
 
 print (json.dumps([[l for l in team if (float(l['latitude'][0:5]), float(l['longitude'][0:5])) in points_twice] for team in teams]))
