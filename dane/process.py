ddb58dc8b7ce6f5742663eabb0cfbd8ac7ad55b5
Paweł Abramowicz <pawelabrams@gmail.com>
2016-06-06 23:14:36 +0200

    Paths, made ready

--- dane/process.py
+++ dane/process.py
@@ -9,18 +9,22 @@ with open('locations_wo_originals.json') as fp:
         last_h = 0
         for l in t:
             p = l['latitude'][0:5] + "," + l['longitude'][0:5]
-            if l['hours_from_start'] > 0 and p != last: # TODO odwrotnie, jeśli minus?!
+            if l['hours_from_start'] > 0 and p != last:
                 points.add(p)
+                if l['hours_from_start'] > last_h:
+                    pathname, hours = last+"-"+p, l['hours_from_start'] - last_h
+                else:
+                    pathname, hours = p+"-"+last, last_h - l['hours_from_start']
                 try:
-                    paths[last+"-"+p].append(l['hours_from_start'] - last_h),
+                    paths[pathname].append(hours),
                 except:
-                    paths[last+"-"+p] = [l['hours_from_start'] - last_h]
+                    paths[pathname] = [hours]
                 last = p
                 last_h = l['hours_from_start']
 
+print (json.dumps(paths))
 
-
-
+"""
 buckets = []
 for x in paths:
     print (x + " " + str(paths[x]))
@@ -31,4 +35,4 @@ print ("3: " + str(buckets.count(3)))
 print ("4: " + str(buckets.count(4)))
 print ("5: " + str(buckets.count(5)))
 print ("6: " + str(buckets.count(6)))
-print ("7+: " + str(sum([x for x in buckets if x > 6])))
+print ("7+: " + str(sum([x for x in buckets if x > 6])))"""
